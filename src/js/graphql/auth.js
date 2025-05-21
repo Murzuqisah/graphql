2bf715aa75e28f5bcc3db26150e8e57bf82d4f12
jamos <amosjoel91@gmail.com>
2025-05-21 15:41:34 +0300

    feat: Implement login functionality with GraphQL mutation

--- src/js/graphql/auth.js
+++ src/js/graphql/auth.js
@@ -3,3 +3,39 @@ const API_ENDPOINT = 'https://learn.zone01kisumu.ke/api/graphql-engine/v1/graphq
 const isAuthenticated = () => {
     return localStorage.getItem('jwt') !== null
 }
+
+// login functionality
+async function login(identifier, password) {
+    const isEmail = identifier.includes('@')
+
+    const query = `
+    mutation {
+    login(
+    ${isEmail ? 'email' : 'username'}: '${identifier}'
+    password: ${password}'
+    ) {
+    token
+    user {
+    id
+    login
+    email
+    }
+    }
+    }
+    `
+
+    try {
+        const loginResponse = await fetch(API_ENDPOINT, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({ query }),
+        })
+        const data = await loginResponse.json()
+
+        if (data.errors) {
+            throw new Error(data.errors[0].message || 'Invalid credentials')
+        }
+    }
+}
