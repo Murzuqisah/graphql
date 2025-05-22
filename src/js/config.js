4755f184e4646897c0ccf04506dfba138bb37e8c
jamos <amosjoel91@gmail.com>
2025-05-22 09:05:59 +0300

    feat: Refactor configuration to export a config object with API and routing keys

--- src/js/config.js
+++ src/js/config.js
@@ -1,1 +1,7 @@
-export const GRAPHQL_API = 'https://learn.zone01kisumu.ke/api/graphql-engine/v1/graphql'
+export const config = {
+    GRAPHQL_API : 'https://learn.zone01kisumu.ke/api/graphql-engine/v1/graphql',
+    jwtKey: 'auth_token',
+    userKey: 'user',
+    defaultRoute: '/login',
+    homeRoute: '/profile',
+};
