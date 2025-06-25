241300751ae072177e8cdd269c585e64c01eac7a
Murzuqisah <amosjoel229@gmail.com>
2025-06-25 23:26:54 +0300

    feat: Implement session check to redirect users based on authentication status

--- src/js/app.js
+++ src/js/app.js
@@ -14,9 +14,22 @@ class App {
         });
     }
 
+    checkExistingSession() {
+        const token = localStorage.getItem('authToken');
+        if (token) {
+            this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+            this.currentPage = 'profile';
+        } else {
+            this.currentPage = 'login';
+        }
+        this.renderCurrentPage();
+    }
+
     renderCurrentPage() {
         if (this.currentPage === 'login') {
             this.LoginPage.render(this.appContainer);
+        } else if (this.currentPage === 'profile') {
+            this.ProfilePage.render(this.appContainer);
         }
     }
 }
