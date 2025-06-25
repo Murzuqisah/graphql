19bd5eb2cb08986dc61cefb8c1c07647feb75cc9
Murzuqisah <amosjoel229@gmail.com>
2025-06-25 23:41:58 +0300

    feat: Add createInput method to DOMUtils for dynamic input element creation

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,6 +6,16 @@ class DOMUtils {
         return element;
     }
 
+    static createInput(type, id, placeholder = '', required = false) {
+        const input = document.createElement('input');
+        input.type = type;
+        input.id = id;
+        input.name = id;
+        input.placeholder = placeholder;
+        input.required = required;
+        return input;
+    }
+
     
 }
 
