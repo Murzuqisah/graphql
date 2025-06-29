2cf1e1ea31c09f285866051d227ca6382a3017fb
Murzuqisah <amosjoel229@gmail.com>
2025-06-30 01:26:35 +0300

    fix: Correct handler instantiation in navigate method of Router class

--- src/js/routes/route.js
+++ src/js/routes/route.js
@@ -7,6 +7,24 @@ export class Router {
 
     addRoute(path, handler) {
         this.routes.set(path, handler);
-    
+
+    }
+
+    setContainer(container) {
+        this.container = container;
+    }
+
+    navigate(path, ...args) {
+        if (!this.routes.has(path)) {
+            return;
+        }
+
+        const Component = this.routes.get(path);
+        this.currentRoute = path;
+
+        if (this.container) {
+            const componentInstance = new handler(...args);
+            componentInstance.show(this.container);
+        }
     }
 }
